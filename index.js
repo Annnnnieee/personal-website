a1ecbc110ecc3184a11ec0ae05e8b15522a365aa
annnnnieee <anastasiya.vt.s@gmail.com>
2019-04-12 16:31:59 -0700

    added basic html templates for the experience and project sections

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ var fs = require("fs");
 var source = fs.readFileSync("./index.html").toString();
 
 var template = Handlebars.compile(source);
-var context = {name: "Arnie", experiences: "dididi", projects: "yes, i did", education: "sigh"};
-var html    = template(context);
+var context = { name: "Arnie", experiences: [
+    { title: "the supermarket", position: "cashsier", date: "forever", description: "I didn't do much", tools: "I used the cash register" },
+    { title: "freelance nothing", position: "bnbbbumming around", date: "yesterday", description: "I didn't do much", tools: "my ass" }
+], projects: [
+    {title: "burnt umberella tumbler", date: "never", description: "in flanders field, the poppies grow. under the stars they lie watching eternity, so far away from it all.", tools: "my ass"},
+    {title: "spleen reticulator", date: "never", description: "my grandmother, all she ever heard were the sweet sounds of the ancients", tools: "the story of my life"}
+], education: "sigh" };
+var html = template(context);
 
-fs.writeFile("output-index.html",html, function(){
+fs.writeFile("output-index.html", html, function () {
     console.log(html);
 })
